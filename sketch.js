3b642258b6e8cec9596bbdbedbfc19714e3bdbf4
Lakshya2510 <74653849+Lakshya2510@users.noreply.github.com>
2021-01-07 14:53:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,48 +1,34 @@
+class Paper {
+    constructor(x, y, r) {
+        var options = {
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1
+        }
 
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+        this.x=x;
+        this.y=y;
+        this.r=r
 
-var engine, world;
-var ground, paper, dustbin1, dustbin2, dustbin3;
+        this.body = Bodies.circle( this.x, this.y, this.r, options);
+        World.add(world,this.body);
+        this.paper = loadImage("paper.png");
 
-function setup() {
-	createCanvas(1200, 700);
+      }
+      display(){
+        var pos =this.body.position;
+        
+        push();
+        translate(pos.x, pos.y);
+        rectMode(CENTER)
+        fill(225,225,225)
+        ellipse( 0,0,this.r);
+        pop();
+        imageMode(CENTER)
+        image(this.paper,pos.x,pos.y,80,80)
+      }
+      
+    };
 
-
-	engine = Engine.create();
-	world = engine.world;
-
-	ground = new Ground(width/2,675,width,20)
-	
-	paper = new Paper(100,500,40)
-	dustbin1 = new Dustbin(900,655,180,20)
-	dustbin2 = new Dustbin(810,618,20,80)
-	dustbin3 = new Dustbin(990,618,20,80)
-
-	Engine.run(engine);
-  
-}
-
-
-function draw() {
-  rectMode(CENTER);
-  background(0);
-  Engine.update(engine);
-  paper.display();
-  dustbin1.display();
-  dustbin2.display();
-  dustbin3.display();
-  ground.display();
-
-  drawSprites();
- 
-}
-
-function keyPressed() {
-	if (keyCode === UP_ARROW) {
-	   Matter.Body.applyForce(paper.body,paper.body.position,{x:230, y:-300});
-	 }
-   }
 
