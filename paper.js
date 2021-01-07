481a3a5af934f8ba26e8fd67ad94bf1c160ecc59
Lakshya2510 <74653849+Lakshya2510@users.noreply.github.com>
2021-01-07 14:53:46 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -13,18 +13,20 @@ class Paper {
 
         this.body = Bodies.circle( this.x, this.y, this.r, options);
         World.add(world,this.body);
+        this.paper = loadImage("paper.png");
+
       }
       display(){
         var pos =this.body.position;
-
+        
         push();
         translate(pos.x, pos.y);
         rectMode(CENTER)
-        strokeWeight(3);
-        fill(255,0,255)
+        fill(225,225,225)
         ellipse( 0,0,this.r);
         pop();
+        imageMode(CENTER)
+        image(this.paper,pos.x,pos.y,80,80)
       }
-
+      
     };
-
